f02a53ed30ea483ca0ac9dd5fe43124091b99788
Cristopher Yates <cmoyates@gmail.com>
2022-07-11 19:33:51 -0230

    Set up Selenium for Heroku

--- selenium_scrape.py
+++ selenium_scrape.py
@@ -3,10 +3,16 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import TimeoutException
+import os
 
-PATH = "chromedriver.exe"
-
-driver = webdriver.Chrome(PATH)
+chrome_options = webdriver.ChromeOptions()
+chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--disable-dev-shm-usage")
+chrome_options.add_argument("--no-sandbox")
+driver = webdriver.Chrome(
+    executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options
+)
 
 
 def get_page_body(company):
